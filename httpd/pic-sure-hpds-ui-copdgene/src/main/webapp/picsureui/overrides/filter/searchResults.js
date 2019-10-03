b3bf7aad7593975d539a83193be9409fa02e26b2
Nathan Chu <chu.nathan@gmail.com>
2019-10-03 15:02:28 -0400

    Filter search results by selected category and subcategory

--- httpd/pic-sure-hpds-ui-copdgene/src/main/webapp/picsureui/overrides/filter/searchResults.js
+++ httpd/pic-sure-hpds-ui-copdgene/src/main/webapp/picsureui/overrides/filter/searchResults.js
@@ -14,50 +14,70 @@ define(["output/outputPanel","picSure/queryBuilder", "filter/searchResult", "han
 		keys.forEach((key) => {
 			var subCategories = [];
 			var categorySearchResultViews = [];
-			$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-				if(data[key]){
-					_.each(data[key], function(value){
-						var newSearchResultRow = new searchResult.View({
-							queryCallback : queryCallback,
-							model : new searchResult.Model(value),
-							filterView: filterView
-						});
-						newSearchResultRow.render();
-						// identify any sub categories, and save them
+			_.each(data[key], function(value){
+				var newSearchResultRow = new searchResult.View({
+					queryCallback : queryCallback,
+					model : new searchResult.Model(value),
+					filterView: filterView
+				});
+				newSearchResultRow.render();
+				// identify any sub categories, and save them
 
-						//trim off leading and trailing slashes.  !! Assume all data starts and ends with '\' !!
-						valuePath = value.data.substr(1, value.data.length-2).split('\\');;
-						// do not add a sub category for leaf nodes.  
-						if(valuePath.length > 2){
-							//console.log('subcategory : ' + valuePath[1]);
-							subCategoryName = valuePath[1];
-							if(subCategories[subCategoryName] ){
-								subCategories[subCategoryName] = subCategories[subCategoryName] + 1;
-							} else {
-								subCategories[subCategoryName] = 1;
-							}
-						} else {
-							//console.log("Leaf : " + value.data);
-						}
-						
-						categorySearchResultViews.push(newSearchResultRow);
-					});
-					console.log("subCategories: " + subCategories);
-					data[key] = undefined;
-
-					if(_.keys(subCategories).length > 1){
-						$('#'+key+'.tab-pane', filterView.$el).append(compiledSubCategoryTemplate(_.keys(subCategories)));
+				//trim off leading and trailing slashes.  !! Assume all data starts and ends with '\' !!
+				valuePath = value.data.substr(1, value.data.length-2).split('\\');;
+				// do not add a sub category for leaf nodes.  
+				if(valuePath.length > 2){
+					//console.log('subcategory : ' + valuePath[1]);
+					subCategoryName = valuePath[1];
+					if(subCategories[subCategoryName] ){
+						subCategories[subCategoryName] = subCategories[subCategoryName] + 1;
+					} else {
+						subCategories[subCategoryName] = 1;
 					}
-
-					$('#'+key+'.tab-pane', filterView.$el).append(_.pluck(categorySearchResultViews, "$el"));
 				}
+				
+				categorySearchResultViews.push(newSearchResultRow);
 			});
+			console.log("subCategories: " + subCategories);
+			data[key] = undefined;
+
+			//save this tab object so we don't keep looking it up
+			var tabPane = $('#'+key+'.tab-pane', filterView.$el)
+
+			if(_.keys(subCategories).length > 1){
+				$(".result-subcategories-div", tabPane).append(compiledSubCategoryTemplate(_.keys(subCategories)));
+				//bootstap.js is used for the top-level category pills; here we are keeping a bit of the naming scheme
+				// need to roll our own logic so that the 'all results' sub-category tab works as expected
+				$(".sub-nav-pills li", tabPane).click(function(event){
+					event.preventDefault();
+					$(event.target.parentElement).addClass("active")
+					$(event.target.parentElement).siblings().removeClass("active");
+					
+					if(event.target.text == "All Results"){
+						_.each(categorySearchResultViews, function(result){
+							result.$el.show();
+						});
+					} else {
+						_.each(categorySearchResultViews, function(result){
+							if(result.model.attributes.parent == event.target.text){
+								result.$el.show();
+							} else {
+								result.$el.hide();
+							}
+						});
+					}
+				});
+			}
+
+
+			$(".search-result-list", tabPane).append(_.pluck(categorySearchResultViews, "$el"));
+
+			
 
 		});
 
 		$("#"+_.first(keys)).addClass("active");
 		$(".nav-pills li:first-child").addClass("active");
-		$('a[data-label="'+_.first(keys)+'"]').trigger('shown.bs.tab')
 
 	}.bind(searchResults);
 
